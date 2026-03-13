b0af29ff9efa80076e06165467da22a97ef05527
billylpz <billyj.96@hotmail.com>
2026-03-13 16:00:40 -0500

    refactor: modified navbar style

--- src/app/admin-dashboard/components/admin-navbar/admin-navbar.component.ts
+++ src/app/admin-dashboard/components/admin-navbar/admin-navbar.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'admin-navbar',
   templateUrl: './admin-navbar.component.html',
-  styleUrls: ['./admin-navbar.component.css']
+  styleUrls: ['./admin-navbar.component.css'],
+  imports: [RouterLink]
 })
 export class AdminNavbarComponent implements OnInit {
 
