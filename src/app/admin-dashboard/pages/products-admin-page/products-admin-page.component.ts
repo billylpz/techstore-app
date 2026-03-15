44277f52fade7108733911a026d9a1637e6cc01c
billylpz <billyj.96@hotmail.com>
2026-03-15 09:54:49 -0500

    feat: add input seach by name for products

--- src/app/admin-dashboard/pages/products-admin-page/products-admin-page.component.ts
+++ src/app/admin-dashboard/pages/products-admin-page/products-admin-page.component.ts
@@ -1,41 +1,68 @@
 import { PaginatorService } from './../../../shared/components/paginator/paginator.service';
-import { Component, computed, EventEmitter, inject, OnInit } from '@angular/core';
+import { Component, computed, DestroyRef, effect, EventEmitter, inject, OnInit, signal } from '@angular/core';
 import { ProductsTableComponent } from '../../../products/components/products-table/products-table.component';
-import { rxResource } from '@angular/core/rxjs-interop';
-import { map, catchError, of, delay } from 'rxjs';
+import { rxResource, takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { map, catchError, of, delay, tap, debounceTime, distinctUntilChanged } from 'rxjs';
 import { ProductService } from '../../../products/service/product.service';
 import { PaginatorComponent } from '../../../shared/components/paginator/paginator.component';
 import { RouterLink } from '@angular/router';
 import Swal from 'sweetalert2';
 import { Product } from '../../../products/interfaces/product.interface';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-products-admin-page',
   templateUrl: './products-admin-page.component.html',
   styleUrls: ['./products-admin-page.component.css'],
-  imports: [ProductsTableComponent, PaginatorComponent, RouterLink]
+  imports: [ProductsTableComponent, PaginatorComponent, RouterLink, ReactiveFormsModule]
 })
-export class ProductsAdminPageComponent {
+export class ProductsAdminPageComponent implements OnInit {
 
+  private destroyRef = inject(DestroyRef);
   private service = inject(ProductService);
   paginatorService = inject(PaginatorService);
 
+  searchByName = new FormControl('');
+  searchResult = signal<string | null>('');
+
+  ngOnInit(): void {
+    this.searchByName.valueChanges.pipe(
+      debounceTime(1000),
+      distinctUntilChanged(), 
+      takeUntilDestroyed(this.destroyRef) 
+    ).subscribe(value => {
+      this.searchResult.set(value?.trim()!)
+      console.log(value);
+    })
+  }
+
+  effects = effect(() => {
+
+  });
+
   productsResource = rxResource({
-    params: () => ({ page: this.paginatorService.currentPage() - 1 }),
+    // Cuando 'page' o 'term' (que son señales) cambien, esto se dispara solo
+    params: () => ({
+      page: this.paginatorService.currentPage() - 1,
+      term: this.searchResult()
+    }),
     stream: ({ params }) => {
-      let page = params.page;
-      return this.service.findAll({ page: page }).pipe(
-        delay(500),
-        catchError(e => of(console.log(e)))
-      );
+      // Si hay texto en el buscador, llamamos al método de búsqueda
+      if (params.term) {
+        return this.service.findAllByName({ page: params.page, term: params.term }).pipe(
+          delay(500)
+        );
+      }
+      // Si no hay texto, llamamos al findAll normal
+      return this.service.findAll({ page: params.page }).pipe(delay(500));
     }
   });
 
 
   eliminarProducto(product: Product) {
-    const isActive= product.active;
+    const isActive = product.active;
     let text = isActive ? 'deshabilitar' : 'habilitar';
-    const request= isActive ?  this.service.delete(product.id!) :  this.service.activate(product.id!);
+    const request = isActive ? this.service.delete(product.id!) : this.service.activate(product.id!);
 
     Swal.fire({
       title: "Advertencia",
@@ -48,15 +75,15 @@ export class ProductsAdminPageComponent {
     }).then((result) => {
       if (result.isConfirmed) {
         request.subscribe({
-          next:()=>{
+          next: () => {
             Swal.fire({
               title: "Aviso!",
-              text: `Producto ${isActive ? 'deshabilitado':'habilitado'}!`,
+              text: `Producto ${isActive ? 'deshabilitado' : 'habilitado'}!`,
               icon: "success"
             });
             this.productsResource.reload();
           },
-          error:(e)=>console.log(e.error)
+          error: (e) => console.log(e.error)
         });
       }
     });
--- src/app/products/service/product.service.ts
+++ src/app/products/service/product.service.ts
@@ -1,12 +1,23 @@
-import {  Injectable } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { Product } from '../interfaces/product.interface';
-import { CommonService } from '../../shared/service/common.service';
+import { CommonService, PageOptions } from '../../shared/service/common.service';
+import { Observable } from 'rxjs';
+import { PageResponse } from '../../shared/interfaces/page-response.interface';
+
+export interface SearchByOptions extends PageOptions {
+  term: string
+}
 
 @Injectable({
   providedIn: 'root'
 })
-export class ProductService extends CommonService<Product>{
-  constructor(){
+export class ProductService extends CommonService<Product> {
+  constructor() {
     super("/api/products")
+  }
+
+  findAllByName(options: SearchByOptions): Observable<PageResponse<Product>> {
+    const { term = '', page = 0 } = options;
+    return this.http.get<PageResponse<Product>>(`${this.urlApi}/by-term`, { params: { page,term } });
   }
 }
