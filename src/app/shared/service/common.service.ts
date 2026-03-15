7ab9e6c8d9547afff8b1361c0003b9d3b6056625
billylpz <billyj.96@hotmail.com>
2026-03-15 10:06:32 -0500

    feat: add pageOptions for http params and catchError on methods

--- src/app/shared/service/common.service.ts
+++ src/app/shared/service/common.service.ts
@@ -1,11 +1,11 @@
 import { inject, Injectable } from '@angular/core';
 import { environment } from '../../../environments/environment';
 import { BaseEntity } from '../interfaces/base-entity.interface';
-import { HttpClient } from '@angular/common/http';
-import { Observable, of } from 'rxjs';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { catchError, Observable, of, throwError } from 'rxjs';
 import { PageResponse } from '../interfaces/page-response.interface';
 
-interface Options {
+export interface PageOptions {
   page?: number;
   size?: number;
 }
@@ -19,45 +19,82 @@ export class CommonService<E extends BaseEntity> {
     this.urlApi = `${environment.API_URL}${endpoint}`;
   }
 
-  findAll(options: Options): Observable<PageResponse<E>> {
+  findAll(options: PageOptions): Observable<PageResponse<E>> {
     const { page = 0, size = 10 } = options
     return this.http.get<PageResponse<E>>(this.urlApi, {
       params: {
         page, size
       }
-    });
+    }).pipe(
+      catchError(e => this.handleErrorMessage(e))
+    );
   }
 
-  findAllActive(options: Options): Observable<PageResponse<E>> {
+  findAllActive(options: PageOptions): Observable<PageResponse<E>> {
     const { page = 0, size = 10 } = options
     return this.http.get<PageResponse<E>>(`${this.urlApi}/active`, {
       params: {
         page, size
       }
-    });
+    }).pipe(
+      catchError(e => this.handleErrorMessage(e))
+    );
   }
 
   findById(id: number | null): Observable<E | null> {
-    if(!id){
+    if (!id) {
       return of(null);
     }
-    return this.http.get<E>(`${this.urlApi}/${id}`);
+    return this.http.get<E>(`${this.urlApi}/${id}`).pipe(
+      catchError(e => this.handleErrorMessage(e))
+    );
   }
 
   save(entity: E): Observable<E> {
-    return this.http.post<E>(this.urlApi, entity);
+    return this.http.post<E>(this.urlApi, entity).pipe(
+      catchError(e => this.handleErrorMessage(e))
+    );
   }
 
   update(entity: E): Observable<E> {
-    return this.http.put<E>(`${this.urlApi}/${entity.id}`, entity);
-  }
-  
-  activate(id:number): Observable<any> {
-    return this.http.patch<any>(`${this.urlApi}/${id}/activate`,null);
+    return this.http.put<E>(`${this.urlApi}/${entity.id}`, entity).pipe(
+      catchError(e => this.handleErrorMessage(e))
+    );
   }
 
-  delete(id:number):Observable<void>{
-    return this.http.delete<void>(`${this.urlApi}/${id}`);
+  activate(id: number): Observable<any> {
+    return this.http.patch<any>(`${this.urlApi}/${id}/activate`, null).pipe(
+      catchError(e => this.handleErrorMessage(e))
+    );
   }
 
+  delete(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.urlApi}/${id}`).pipe(
+      catchError(e => this.handleErrorMessage(e))
+    );
+  }
+
+
+  protected handleErrorMessage(error: HttpErrorResponse) {
+    let errorMessage = 'Ocurrió un error inesperado!';
+
+    if (error.status === 400) {
+      const errorBody = error.error;
+
+      // Caso A: Es el mapa de errores de validación de campos (BindingResult)
+      if (typeof errorBody === 'object' && !errorBody.message) {
+        // Extraemos los valores del objeto y los unimos con saltos de línea
+        errorMessage = JSON.stringify(errorBody);
+      }
+      // Caso B: Es tu excepción personalizada (DuplicateEntityException)
+      else {
+        errorMessage = errorBody?.message || errorMessage;
+      }
+    }
+    else {
+      errorMessage = error.error.message;
+    }
+
+    return throwError(() => errorMessage);
+  }
 }
