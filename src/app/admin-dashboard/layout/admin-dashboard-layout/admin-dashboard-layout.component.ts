fd22439b1717867a39a3b39606c9eb8df1025943
billylpz <billyj.96@hotmail.com>
2026-03-21 18:28:24 -0500

    feat: add footer shared component - also added py-15 to home admin page container

--- src/app/admin-dashboard/layout/admin-dashboard-layout/admin-dashboard-layout.component.ts
+++ src/app/admin-dashboard/layout/admin-dashboard-layout/admin-dashboard-layout.component.ts
@@ -1,12 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { AdminNavbarComponent } from '../../components/admin-navbar/admin-navbar.component';
 import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from "../../../shared/components/footer/footer.component";
 
 @Component({
   selector: 'app-admin-dashboard-layout',
   templateUrl: './admin-dashboard-layout.component.html',
   styleUrls: ['./admin-dashboard-layout.component.css'],
-  imports: [AdminNavbarComponent, RouterOutlet]
+  imports: [AdminNavbarComponent, RouterOutlet, FooterComponent]
 })
 export class AdminDashboardLayoutComponent implements OnInit {
 
