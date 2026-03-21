2a5cb3bf0a11a57cd26c15d4234d8777d514a50a
billylpz <billyj.96@hotmail.com>
2026-03-21 13:46:36 -0500

    feat: add cache to service methods

--- src/app/products/service/product.service.ts
+++ src/app/products/service/product.service.ts
@@ -1,7 +1,7 @@
 import { Product } from './../interfaces/product.interface';
 import { Injectable } from '@angular/core';
 import { CommonService, PageOptions } from '../../shared/service/common.service';
-import { catchError, Observable } from 'rxjs';
+import { catchError, delay, Observable, of, tap } from 'rxjs';
 import { PageResponse } from '../../shared/interfaces/page-response.interface';
 
 export interface SearchByOptions extends PageOptions {
@@ -18,30 +18,52 @@ export class ProductService extends CommonService<Product> {
 
   findAllByName(options: SearchByOptions): Observable<PageResponse<Product>> {
     const { term = '', page = 0 } = options;
-    return this.http.get<PageResponse<Product>>(`${this.urlApi}/by-term`, { params: { page, term } });
+    const key = `cache-${this.apiPath}-${page}-${term}`;
+
+    if (sessionStorage.getItem(key)) {
+      const response = JSON.parse(sessionStorage.getItem(key) || "{}");
+      if (response.content != null && response.content.length > 0) {
+        return of(response).pipe(delay(300));
+      }
+    }
+
+    return this.http.get<PageResponse<Product>>(`${this.urlApi}/by-term`, { params: { page, term } }).pipe(
+      tap(response => {
+        sessionStorage.setItem(key, JSON.stringify(response))
+      }),
+      catchError(e => this.handleErrorMessage(e))
+    );
   }
 
   saveWithImages(product: Product, files: File[]): Observable<Product> {
-    const formData = new FormData()
-    formData.append("name", product.name)
-    formData.append("description", product.description)
-    formData.append("price", product.price.toString())
-    formData.append("stock", product.stock.toString())
-    formData.append("category.id", product.category?.id?.toString() || '0')
-    formData.append("brand.id", product.brand?.id?.toString() || '0')
-
-    if (files != null && files.length > 0) {
-      files.forEach(file => {
-        formData.append('files', file);
-      });
-    }
+    const formData = this.buildFormData(product, files);
 
     return this.http.post<Product>(`${this.urlApi}/with-images`, formData).pipe(
+      tap(() => this.clearCache()),
       catchError(e => this.handleErrorMessage(e))
     );
   }
 
+
   updateWithImages(product: Product, files: File[]): Observable<Product> {
+    const formData = this.buildFormData(product, files);
+
+    return this.http.put<Product>(`${this.urlApi}/with-images/${product.id}`, formData).pipe(
+      tap(() => this.clearCache()),
+      catchError(e => this.handleErrorMessage(e))
+    );
+  }
+
+
+  deleteProductImage(publicId: string): Observable<void> {
+    return this.http.delete<void>(`${this.urlApi}/product-image`, { params: { publicId } }).pipe(
+      tap(() => this.clearCache()),
+      catchError(e => this.handleErrorMessage(e))
+    );
+  }
+
+  ///helpers
+  private buildFormData(product: Product, files: File[]): FormData {
     const formData = new FormData()
     formData.append("name", product.name)
     formData.append("description", product.description)
@@ -55,16 +77,6 @@ export class ProductService extends CommonService<Product> {
         formData.append('files', file);
       });
     }
-
-    return this.http.put<Product>(`${this.urlApi}/with-images/${product.id}`, formData).pipe(
-      catchError(e => this.handleErrorMessage(e))
-    );
-  }
-
-
-  deleteProductImage(publicId:string):Observable<void>{
-    return this.http.delete<void>(`${this.urlApi}/product-image`, {params:{publicId}}).pipe(
-      catchError(e=>this.handleErrorMessage(e))
-    );
+    return formData;
   }
 }
--- src/app/shared/service/common.service.ts
+++ src/app/shared/service/common.service.ts
@@ -2,7 +2,7 @@ import { inject, Injectable } from '@angular/core';
 import { environment } from '../../../environments/environment';
 import { BaseEntity } from '../interfaces/base-entity.interface';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
-import { catchError, Observable, of, throwError } from 'rxjs';
+import { catchError, delay, Observable, of, tap, throwError } from 'rxjs';
 import { PageResponse } from '../interfaces/page-response.interface';
 
 export interface PageOptions {
@@ -12,31 +12,58 @@ export interface PageOptions {
 
 
 export class CommonService<E extends BaseEntity> {
-  protected urlApi = environment.API_URL;
+  protected urlApi;
   protected http = inject(HttpClient);
+  protected apiPath;
 
   constructor(endpoint: string) {
     this.urlApi = `${environment.API_URL}${endpoint}`;
+    this.apiPath = endpoint;
   }
 
+
   findAll(options: PageOptions): Observable<PageResponse<E>> {
-    const { page = 0, size = 10 } = options
+    const { page = 0, size = 5 } = options;
+    const key = `cache-${this.apiPath}-${page}-${size}`;
+
+    if (sessionStorage.getItem(key)) {
+      const response = JSON.parse(sessionStorage.getItem(key) || "{}");
+      if (response.content != null && response.content.length > 0) {
+        return of(response).pipe(delay(300));
+      }
+    }
+
     return this.http.get<PageResponse<E>>(this.urlApi, {
       params: {
         page, size
       }
     }).pipe(
+      tap(response => {
+        sessionStorage.setItem(key, JSON.stringify(response))
+      }),
       catchError(e => this.handleErrorMessage(e))
     );
   }
 
   findAllActive(options: PageOptions): Observable<PageResponse<E>> {
-    const { page = 0, size = 10 } = options
+    const { page = 0, size = 10 } = options;
+    const key = `cache-${this.apiPath}-${page}-${size}-active`;
+
+    if (sessionStorage.getItem(key)) {
+      const response = JSON.parse(sessionStorage.getItem(key) || "{}");
+      if (response.content != null && response.content.length > 0) {
+        return of(response).pipe(delay(300));
+      }
+    }
+
     return this.http.get<PageResponse<E>>(`${this.urlApi}/active`, {
       params: {
         page, size
       }
     }).pipe(
+      tap(response => {
+        sessionStorage.setItem(key, JSON.stringify(response))
+      }),
       catchError(e => this.handleErrorMessage(e))
     );
   }
@@ -52,29 +79,42 @@ export class CommonService<E extends BaseEntity> {
 
   save(entity: E): Observable<E> {
     return this.http.post<E>(this.urlApi, entity).pipe(
+      tap(() => this.clearCache()),
       catchError(e => this.handleErrorMessage(e))
     );
   }
 
   update(entity: E): Observable<E> {
     return this.http.put<E>(`${this.urlApi}/${entity.id}`, entity).pipe(
+      tap(() => this.clearCache()),
       catchError(e => this.handleErrorMessage(e))
     );
   }
 
   activate(id: number): Observable<any> {
     return this.http.patch<any>(`${this.urlApi}/${id}/activate`, null).pipe(
+      tap(() => this.clearCache()),
       catchError(e => this.handleErrorMessage(e))
     );
   }
 
   delete(id: number): Observable<void> {
     return this.http.delete<void>(`${this.urlApi}/${id}`).pipe(
+      tap(() => this.clearCache()),
       catchError(e => this.handleErrorMessage(e))
     );
   }
 
+  //---------helpers
 
+  //elimina la caché de un recurso cuando se hace un save/update/delete de la entidad correspondiente
+  protected clearCache(): void {
+    Object.keys(sessionStorage).filter(key => key.startsWith(`cache-${this.apiPath}`))
+      .forEach(key => sessionStorage.removeItem(key));
+    console.log("cache borrada");
+  }
+
+  //extrae el message del objeto error del backend y lo devuelve como string envuelto en throw()
   protected handleErrorMessage(error: HttpErrorResponse) {
     let errorMessage = 'Ocurrió un error inesperado!';
 
