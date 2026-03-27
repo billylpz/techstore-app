f814c706c0af1392a8ccc53097ce19799d2c6b53
billylpz <billyj.96@hotmail.com>
2026-03-27 12:27:25 -0500

    feat(formUtils): add errors validators

--- src/app/shared/utils/form-utils.ts
+++ src/app/shared/utils/form-utils.ts
@@ -1,9 +1,11 @@
-import { AbstractControl, ValidationErrors } from "@angular/forms";
+import { AbstractControl, ValidationErrors, ValidatorFn } from "@angular/forms";
 
 export class FormUtils {
-    static cellphonePattern = '^[0-9]+$';
-    static pricePattern = '^[0-9]+([\\.,][0-9]{1,2})?$';
-    static numberPattern = '^[0-9]+$';
+    static readonly cellphonePattern = '^[0-9]+$';
+    static readonly pricePattern = '^[0-9]+([\\.,][0-9]{1,2})?$';
+    static readonly numberPattern = '^[0-9]+$';
+    static readonly emailPattern = '^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$';
+
 
     static getTextError(formControl: AbstractControl): string | null {
         if (formControl == null) {
@@ -22,6 +24,8 @@ export class FormUtils {
                 switch (key) {
                     case 'required':
                         return 'Este campo es requerido'
+                    case 'forbiddenValue':
+                        return `'Este campo no puede contener la palabra "${errors['forbiddenValue'].value}"'`
                     case "minlength":
                         return `Mínimo de ${errors['minlength'].requiredLength} carácteres requeridos`;
                     case "maxlength":
@@ -35,7 +39,7 @@ export class FormUtils {
 
                         if (pattern === FormUtils.pricePattern) {
                             return "Formato inválido. Use máximo 2 decimales (ej: 10.99)";
-                        }if (pattern === FormUtils.numberPattern) {
+                        } if (pattern === FormUtils.numberPattern) {
                             return "Solo se permiten números enteros";
                         }
                         return "Solo se permiten números";
@@ -49,5 +53,29 @@ export class FormUtils {
         return null;
     }
 
-   
+
+    //errors validations
+
+    static passwordMatchValidator(control: AbstractControl): ValidationErrors | null {
+        const password = control.get('password');
+        const confirmPassword = control.get('confirmPassword');
+
+        if (password && confirmPassword && password.value !== confirmPassword.value) {
+            confirmPassword.setErrors({ mismatch: true });
+            return { mismatch: true };
+        }
+
+        return null;
+    }
+
+    static forbiddenValues(): ValidatorFn {
+        const FORBIDDEN_VALUES: string[] = ['admin', 'root',];
+        return (control: AbstractControl): ValidationErrors | null => {
+            const value = control.value.trim().toLowerCase() as string;
+            const invalidValue = FORBIDDEN_VALUES.find(v => value.includes(v.trim()));
+
+            return invalidValue ? { forbiddenValue: { value: invalidValue } } : null;
+        }
+    }
+
 }
