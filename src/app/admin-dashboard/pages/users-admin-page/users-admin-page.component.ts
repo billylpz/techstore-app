f4ed433e6bf54543f10018def5e66453c6cfb0de
billylpz <billyj.96@hotmail.com>
2026-03-30 13:24:35 -0500

    feat(users): add searchByNameOrLastname methods

--- src/app/admin-dashboard/pages/users-admin-page/users-admin-page.component.ts
+++ src/app/admin-dashboard/pages/users-admin-page/users-admin-page.component.ts
@@ -1,31 +1,65 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, effect, inject, signal } from '@angular/core';
 import { User } from '../../../users/interfaces/user.interface';
 import { rxResource } from '@angular/core/rxjs-interop';
-import { delay, catchError, of } from 'rxjs';
+import { delay, catchError, of, debounceTime, distinctUntilChanged, tap, EMPTY } from 'rxjs';
 import Swal from 'sweetalert2';
 import { PaginatorService } from '../../../shared/components/paginator/paginator.service';
 import { UserService } from '../../../users/services/user.service';
 import { PaginatorComponent } from "../../../shared/components/paginator/paginator.component";
 import { UsersTableComponent } from "../../../users/components/users-table/users-table.component";
-import { RouterLink } from '@angular/router';
+import { ActivatedRoute, Router, RouterLink } from '@angular/router';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-users-admin-page',
   templateUrl: './users-admin-page.component.html',
   styleUrls: ['./users-admin-page.component.css'],
-  imports: [PaginatorComponent, UsersTableComponent,RouterLink]
+  imports: [PaginatorComponent, UsersTableComponent, RouterLink, ReactiveFormsModule]
 })
 export class UsersAdminPageComponent {
   private service = inject(UserService);
   paginatorService = inject(PaginatorService);
+  router = inject(Router);
+  route = inject(ActivatedRoute);
+  searchByInput = new FormControl<string>('');
+  searchByValue = signal<string>('');
+
+  constructor() {
+    this.searchByInput.valueChanges.pipe(
+      debounceTime(1000),
+      distinctUntilChanged())
+      .subscribe(value => {
+        this.searchByValue.set(value!.trim());
+
+        //reseteamos el param page a 1
+        this.router.navigate([], {
+          relativeTo: this.route,
+          queryParams: { page: 1 },
+          queryParamsHandling: 'merge'
+        });
+      })
+  };
+
+  effects = effect(() => {
+    const error = this.usersResource.error();
+    if (error) {
+      Swal.fire("Error de Red", String(error), "error");
+    }
+  });
 
   usersResource = rxResource({
-    params: () => ({ page: this.paginatorService.currentPage() - 1 }),
+    params: () => ({ page: this.paginatorService.currentPage() - 1, term: this.searchByValue() }),
     stream: ({ params }) => {
       let page = params.page;
+      let term = params.term;
+
+      if (term && term.length > 0) {
+        return this.service.findAllByNameOrLastname({ page, term }).pipe(
+          delay(500)
+        );
+      }
       return this.service.findAll({ page: page }).pipe(
-        delay(500),
-        catchError(e => of(console.log(e)))
+        delay(500)
       );
     }
   });
--- src/app/users/services/user.service.ts
+++ src/app/users/services/user.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
-import { CommonService } from '../../shared/services/common.service';
+import { CommonService, PageOptions, SearchByOptions } from '../../shared/services/common.service';
 import { User } from '../interfaces/user.interface';
+import { PageResponse } from '../../shared/interfaces/page-response.interface';
+import { delay, Observable, of, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -11,4 +13,26 @@ export class UserService extends CommonService<User> {
     super("/api/users")
   }
 
+
+   findAllByNameOrLastname(options: SearchByOptions): Observable<PageResponse<User>> {
+      const { page = 0, size = 3, term='' } = options;
+      const key = `cache-${this.apiPath}-${page}-${size}-${term}`;
+  
+      if (sessionStorage.getItem(key)) {
+        const response = JSON.parse(sessionStorage.getItem(key) || "{}");
+        if (response.content != null && response.content.length > 0) {
+          return of(response).pipe(delay(300));
+        }
+      }
+  
+      return this.http.get<PageResponse<User>>(`${this.urlApi}/by-term`, {
+        params: {
+          page, size, term
+        }
+      }).pipe(
+        tap(response => {
+          sessionStorage.setItem(key, JSON.stringify(response))
+        }),
+      );
+    }
 }
