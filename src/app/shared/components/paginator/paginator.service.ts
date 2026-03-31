0a4034dd3a49139a4a82c963226a3cb7112f83fd
billylpz <billyj.96@hotmail.com>
2026-03-31 14:41:58 -0500

    feat(paginator): add reset page method

--- src/app/shared/components/paginator/paginator.service.ts
+++ src/app/shared/components/paginator/paginator.service.ts
@@ -1,6 +1,6 @@
 import { inject, Injectable } from '@angular/core';
 import { toSignal } from '@angular/core/rxjs-interop';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { map } from 'rxjs';
 
 @Injectable({
@@ -8,13 +8,23 @@ import { map } from 'rxjs';
 })
 export class PaginatorService {
   route = inject(ActivatedRoute);
+  router = inject(Router);
 
   currentPage = toSignal(this.route.queryParamMap.pipe(
     map(params => {
       const pageParam = params.get('page');
       const pageNumber = Number(pageParam);
 
-      return (isNaN(pageNumber) || pageNumber<=0)? 1 : pageNumber;
+      return (isNaN(pageNumber) || pageNumber <= 0) ? 1 : pageNumber;
     })
-  ),{initialValue:1})
+  ), { initialValue: 1 });
+
+
+  reset(): void {
+    this.router.navigate([], {
+      relativeTo: this.route,
+      queryParams: { page: 1 },
+      queryParamsHandling: 'merge'
+    });
+  }
 }
