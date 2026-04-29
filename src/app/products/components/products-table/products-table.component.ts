f7e3bc4d5ecf0d3af13113c0da89cc087f6d0bda
billylpz <billyj.96@hotmail.com>
2026-04-29 13:15:26 -0500

    feat: add mobile breakpoints and improve styles

--- src/app/products/components/products-table/products-table.component.ts
+++ src/app/products/components/products-table/products-table.component.ts
@@ -1,21 +1,21 @@
-import { Component, inject, input, OnInit, output } from '@angular/core';
+import { Component, input, output } from '@angular/core';
 
 import { Product } from '../../interfaces/product.interface';
 import { DatePipe, NgClass } from '@angular/common';
 import { RouterLink } from '@angular/router';
-import {  ActiveTextEntityPipe } from "../../../shared/pipes/active-text-entity.pipe";
-import {ActiveStateColorPipe } from "../../../shared/pipes/active-state-color.pipe";
+import { ActiveTextEntityPipe } from "../../../shared/pipes/active-text-entity.pipe";
+import { ActiveStateColorPipe } from "../../../shared/pipes/active-state-color.pipe";
 @Component({
   selector: 'products-table',
   templateUrl: './products-table.component.html',
   styleUrls: ['./products-table.component.css'],
-  imports: [DatePipe, RouterLink, NgClass,  ActiveStateColorPipe, ActiveTextEntityPipe]
+  imports: [DatePipe, RouterLink, NgClass, ActiveStateColorPipe, ActiveTextEntityPipe]
 })
 export class ProductsTableComponent {
- products = input.required<Product[] | undefined>();
- onDelete=output<Product>()
+  products = input.required<Product[] | undefined>();
+  onDelete = output<Product>()
 
- eliminar(product:Product){
-  this.onDelete.emit(product);
- }
+  eliminar(product: Product) {
+    this.onDelete.emit(product);
+  }
 }
